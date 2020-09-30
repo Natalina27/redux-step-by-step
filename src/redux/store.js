e88fc99d9624170cc6a6da42d4343b8c94b3699d
Natalya Myunster <natalyamyunster@yahoo.com>
2020-09-30 17:10:38 +0200

    index.js: added Provider store.js: added Reducer

---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import {createStore} from 'redux';
+
+// Reducer to-do
+const initialState = {};
+const todo = (state = initialState, action) => {
+};
+
+//
+const store = createStore(
+    todo,
+
+);
+
+export default store;

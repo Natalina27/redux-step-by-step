4236cf9205807c0cbed7c05d16b3df733a4aff4c
Natalya Myunster <natalyamyunster@yahoo.com>
2020-09-30 13:11:03 +0200

    refactoring

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import s from '../App/App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      React app
+    <div className={s.App}>
+     REDUX STEP BY STEP
     </div>
   );
 }
